1b4a4a6bab2121c67cc036b16a2ff07775050baa
Meyau <2403311756@students.satyaterrabhinneka.ac.id>
2025-06-22 23:15:44 +0700

    Dari folder data

---
+++ booksData.js
@@ -0,0 +1,52 @@
+const booksData = [
+  {
+    id: "book1",
+    imageUrl: "menguasai_pemrograman_berorientasi_objek.jpg",
+    title: "Menguasai Pemrograman Berorientasi Objek",
+    author: "Ade Rahmat Iskandar",
+    publisher: "Informatika",
+    year: "2020",
+  },
+  {
+    id: "book2",
+    imageUrl: "dasar_dasar_pemrograman_net.jpg",
+    title: "Dasar-Dasar Pemrograman dengan .NET",
+    author: "Ade Rahmat Iskandar",
+    publisher: "Informatika",
+    year: "2019",
+  },
+  {
+    id: "book3",
+    imageUrl: "metodologi_pengembangan_sistem_informasi.jpg",
+    title: "Metodologi Pengembangan Sistem Informasi",
+    author: "Samaji Sarosa",
+    publisher: "Indeks",
+    year: "2017",
+  },
+  {
+    id: "book4",
+    imageUrl: "struktur_data.jpg",
+    title: "Struktur Data",
+    author: "Rosa A.S",
+    publisher: "Modula",
+    year: "2018",
+  },
+  {
+    id: "book5",
+    imageUrl: "Dasar_pemrograman_python_3 - Copy.jpg",
+    title: "Dasar Pemrograman Python 3",
+    author: "Abdul Kadir",
+    publisher: "Andi Publisher",
+    year: "2018",
+  },
+  {
+    id: "book6",
+    imageUrl: "sistem_basis_data_sql.jpg",
+    title: "Sistem Basis Data Dan Sql",
+    author: "Didik Setyadi",
+    publisher: "Mitra Wacana Media",
+    year: "2020",
+  },
+];
+
+export default booksData;
